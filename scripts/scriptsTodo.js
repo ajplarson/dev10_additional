49956fb9abc067d6b0976beb8add2b8a8cca82f7
Andrew Larson <andrewjplarson@gmail.com>
2019-04-27 22:03:07 -0500

    added a lot but no functionality currently

---
+++ scripts/scriptsTodo.js
@@ -0,0 +1,70 @@
+window.onload=function() {
+    var idVariable = 1;
+    var inputForm =document.getElementById("inputForm");
+    var daysToComplete = document.getElementById("daysToComplete");
+    var todoTask = document.getElementById("todoTask");
+    var submitButton = document.getElementById("submitButton");
+    var listOutput = document.getElementById("listOutput");
+
+    submitButton.addEventListener("click", submitTask);
+
+
+    function submitTask() {
+        //input validation for task
+        if(todoTask === "") {
+            alert("Please fill out this field.");
+        }
+        else {
+            var enteredTask = todoTask.nodeValue;
+            var item = `<li id="li-${id}">${enteredTask}
+            `
+        }
+
+    }
+
+
+
+
+    /*
+    //Form handling from number chart code along
+function clearErrors() {    
+    for (var loopCounter = 0; 
+        loopCounter < document.forms["luckySevens"].elements.length; 
+        loopCounter++) {
+        if (document.forms["luckySevens"].elements[loopCounter]
+           .parentElement.className.indexOf("has-") != -1) {
+            
+            document.forms["luckySevens"].elements[loopCounter]
+               .parentElement.className = "form-group";
+        }
+    }    
+} 
+function resetForm() {
+    clearErrors();
+    document.forms["luckySevens"]["bet"].value = "";
+    document.getElementById("results").style.display = "none";
+    document.getElementById("submitButton").innerText = "Submit";
+    document.forms["luckySevens"]["bet"].focus();
+}
+function validateItems() {
+    clearErrors();
+    var bet = Number(document.forms["luckySevens"]["bet"].value); 
+    if (bet == "" || isNaN(bet) || bet <=0 || Number.isInteger(bet) != true) {
+        alert("Bet must be a whole number greater than zero.");
+        document.forms["luckySevens"]["bet"]
+           .parentElement.className = "form-group has-error";
+        document.forms["luckySevens"]["bet"].focus();
+        return false;
+    }
+	var luckyArray = luckySevens(bet);
+   document.getElementById("results").style.display = "block";
+   document.getElementById("submitButton").innerText = "Play";
+   document.getElementById("startingBet").innerText = Number(bet);
+   document.getElementById("totalRolls").innerText = luckyArray[0];
+   document.getElementById("highestAmount").innerText = luckyArray[1];
+   document.getElementById("challengingResult").innerText = luckyArray[2];
+   // We are returning false so that the form doesn't submit 
+   // and so that we can see the results
+   return false;
+} */
+}
